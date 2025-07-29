3c66ba407107a5c21be488ab5bf2ca5e3de695e8
kemanisann <louvainwebsite@gmail.com>
2025-07-29 12:24:42 +0700

    dsad

--- schema.sql
+++ schema.sql
@@ -7,11 +7,11 @@ CREATE TABLE products (
   ring_width_pria DECIMAL(4,2),
   gem_wanita TEXT,
   gem_pria  TEXT
-)
+);
 
 CREATE TABLE product_images (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   product_id TEXT NOT NULL,
   image_url TEXT NOT NULL,
   FOREIGN KEY (product_id) REFERENCES products(id)
-)
+);
