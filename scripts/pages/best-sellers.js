0902eb13856ef513d4c41c68f1fd18aeb92bfa6f
kemanisann <louvainwebsite@gmail.com>
2025-07-29 11:43:41 +0700

    fdfd

--- scripts/pages/best-sellers.js
+++
@@ -1,7 +0,0 @@
-import { renderVideos } from "../layout/best-sellers/render-videos.js";
-import { renderDesign } from "../layout/render-design.js";
-import { renderPageHero } from "../layout/render-page-hero.js";
-
-renderPageHero();
-renderDesign({ slice: false });
-renderVideos();
--- scripts/pages/favorite-page.js
+++
@@ -1,6 +0,0 @@
-import { renderFavorite } from "../layout/favorite/render-favorite.js";
-import { loadFavorites, updateFavoriteIcon } from "../utils/favorites.js";
-
-renderFavorite();
-loadFavorites();
-updateFavoriteIcon();
