c3fb7a9e182bd9bbba8b66dc9d698f183665e532
kemanisann <louvainwebsite@gmail.com>
2025-07-31 13:47:55 +0700

    kj

--- schema.sql
+++ schema.sql
@@ -1,17 +1,12 @@
-CREATE TABLE products (
+CREATE TABLE users (
   id TEXT PRIMARY KEY,
-  collection TEXT NOT NULL,
-  name TEXT,
-  price TEXT,
-  ring_width_wanita DECIMAL(4,2),
-  ring_width_pria DECIMAL(4,2),
-  gem_wanita TEXT,
-  gem_pria  TEXT
+  email TEXT UNIQUE NOT NULL,
+  password_hash TEXT,
+  role TEXT NOT NULL
 );
 
-CREATE TABLE product_images (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  product_id TEXT NOT NULL,
-  image_url TEXT NOT NULL,
-  FOREIGN KEY (product_id) REFERENCES products(id)
+CREATE TABLE carts (
+  id TEXT PRIMARY KEY,
+  user_id TEXT,
+  items TEXT
 );
